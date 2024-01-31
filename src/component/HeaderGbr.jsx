649c6734d4e4dee7e510cb070778836541b3f4ab
BayuKartawan <kartawanbayu@gmail.com>
2024-01-31 18:32:01 +0800

    gambar header

--- src/component/HeaderGbr.jsx
+++ src/component/HeaderGbr.jsx
@@ -1,11 +1,18 @@
 const HeaderGbr = () => {
     return (
         <section className="h-gbr">
-            <div className="w-layout-blockcontainer container-3 w-container"><img
-                src="https://assets-global.website-files.com/65b3ea448c2bd342c4b99cf7/65b3edbe0c1c567dcb83a5c2_IMG-20240126-WA0017.jpg"
-                loading="lazy" sizes="(max-width: 1600px) 100vw, 1600px"
-                srcSet="https://assets-global.website-files.com/65b3ea448c2bd342c4b99cf7/65b3edbe0c1c567dcb83a5c2_IMG-20240126-WA0017-p-500.jpg 500w, https://assets-global.website-files.com/65b3ea448c2bd342c4b99cf7/65b3edbe0c1c567dcb83a5c2_IMG-20240126-WA0017-p-800.jpg 800w, https://assets-global.website-files.com/65b3ea448c2bd342c4b99cf7/65b3edbe0c1c567dcb83a5c2_IMG-20240126-WA0017-p-1080.jpg 1080w, https://assets-global.website-files.com/65b3ea448c2bd342c4b99cf7/65b3edbe0c1c567dcb83a5c2_IMG-20240126-WA0017.jpg 1600w"
-                alt="" className="image" /></div>
+            <div className="w-layout-blockcontainer container-3 w-container">
+                <img
+                    src="/pelantikan/pelantikan9.jpg"
+                    loading="lazy" sizes="(max-width: 1600px) 100vw, 1600px"
+                    srcSet="/pelantikan/pelantikan9.jpg 500w, 
+                    /pelantikan/pelantikan9.jpg 800w, 
+                    /pelantikan/pelantikan9.jpg 1080w, 
+                    /pelantikan/pelantikan9.jpg 1600w"
+                    alt=""
+                    className="image"
+                />
+            </div>
         </section>
     )
 }
